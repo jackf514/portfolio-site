54451e12e2a84b2c42d84c15be74b5acca7cea6c
jackf514 <jackferguson514@gmail.com>
2022-11-17 09:32:51 -0600

    begin creating fun facts in JS, minor CSS fix

---
+++ script.js
@@ -0,0 +1,7 @@
+const randomFacts = [
+    'One light year is  a length spanning 5 trillion, 903 billion, 026 million, 326 thousand, and 255 miles',
+    'There is an area around a black hole called the Photon Sphere where light can orbit a black hole. This means that, if you were to stand in that area, you could theoretically see the back of your own head',
+    'Light tends to act like a wave, unless we try to directly observe it, then it behaves like a particle instead',
+    'There are more stars in the sky than grains of sand on the Earth',
+    'Some butterflys have nano-scale baffle structures in their wings that allow them to trap specific wavelengths of light, giving them their unique and vibrant colors'
+]
