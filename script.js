1308601ecb9c20ed1d39dbba48d3526f98a570e5
jackf514 <jackferguson514@gmail.com>
2022-11-21 11:34:14 -0600

    add randomly selected fun facts via JS

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
+/* Fun Fact Section*/
 const randomFacts = [
     'One light year is a length spanning 5 trillion, 903 billion, 26 million, 326 thousand, and 255 miles',
     'There is an area around a black hole called the Photon Sphere where light can orbit a black hole. This means that, if you were to stand in that area, you could theoretically see the back of your own head',
     'Light tends to act like a wave, unless we try to directly observe it, then it behaves more like a particle instead',
     'There are more stars in the sky than grains of sand on the Earth',
     'Some butterflys have nano-scale baffle structures in their wings that allow them to trap specific wavelengths of light, giving them their unique and vibrant colors'
-]
+];
+
+const getFunFact = (randFacts) => {
+    let funFact = randFacts[Math.floor(Math.random()*randFacts.length)];
+    console.log(funFact);
+    return funFact
+}
+
+document.getElementById("fact").innerHTML = getFunFact(randomFacts);
+/*-----------*/
+
+/* Replace Stylesheet Section */
+
+/*----------*/
