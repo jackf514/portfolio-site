52488a417ca947f8d00a35a48602f1886c5b5561
jackf514 <jackferguson514@gmail.com>
2022-11-22 11:03:21 -0600

    finalize js code to switch stylesheets

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 /* Fun Fact Section*/
 const randomFacts = [
     'One light year is a length spanning 5 trillion, 903 billion, 26 million, 326 thousand, and 255 miles',
-    'There is an area around a black hole called the Photon Sphere where light can orbit a black hole. This means that, if you were to stand in that area, you could theoretically see the back of your own head',
+    'There is an area around a black hole called the Photon Sphere where light can orbit a black hole. This means that, if you were to stand in that area, you could theoretically see the back of your own head!',
     'Light tends to act like a wave, unless we try to directly observe it, then it behaves more like a particle instead',
     'There are more stars in the sky than grains of sand on the Earth',
     'Some butterflys have nano-scale baffle structures in their wings that allow them to trap specific wavelengths of light, giving them their unique and vibrant colors'
@@ -21,7 +21,7 @@ const selectStyle = {
         href: './CSS_styles/styles.css',
         description: 'My original styling. This is what I came up with without any styling guidelines or restraints'
     },
-    default_lt: {
+    defaultLT: {
         href: './CSS_styles/default_lt.css',
         description: 'My original styling but in a light theme rather than a dark theme. Only the colors have changed'
     },
@@ -36,11 +36,11 @@ const selectStyle = {
 };
 
 
-const changeStyle = () => {
-    let styleChoice = '';
-    styleChoice = document.getElementById("style").value;
-    document.getElementById('varStyle').href = selectStyle.styleChoice.href;
+const changeStyle = (styleList) => {
+    let styleChoice = document.getElementById("style").value;
+    document.getElementById('varStyle').href = styleList[styleChoice].href;
+    document.getElementById('style-desc').innerHTML = styleList[styleChoice].description;
 }
 
-document.getElementById("style").addEventListener("change", changeStyle);
+document.getElementById("style").addEventListener("change", function(){ changeStyle(selectStyle) });
 /*----------*/
